4b9057cb497dacb99a53060ba3cce6d747815512
Taj485 <tajbi21@gmail.com>
2019-12-10 21:59:09 +0000

    S001 - added ViewModel folder

---
+++ ShoppingSpree.Data/ViewModels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Drawing;
+
+namespace ShoppingSpree.Data.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int productId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductColour { get; set; }
+        public int ProductPrice { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+        public string ConvertedImgSrc { get; set; }
+    }
+}
